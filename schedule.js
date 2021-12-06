d26820c0872db79f58b1f6408c9a19bc67dfad8a
Mona <you@example.com>
2021-12-05 21:00:29 -0500

    attempted to add time slots and bootstap

---
+++ schedule.js
@@ -0,0 +1,12 @@
+let d = new Date()
+
+let year = d.getFullYear()
+let month = d.getMonth()+1;
+let day = d.getDate();
+
+let totalDays = new Date(year, month, 0).getDate()
+
+let output =
+
+(month<10 ? '0': '') + month + '/' +
+(day<10 ? '0' : '') + day + '/' + year
